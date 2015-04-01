ddbacd483878f7f3ed40453adffec985ad2a0c9d
mbouzi <mrbouzi@gmail.com>
2015-04-01 09:35:03 -0400

    following  contraints/validations

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,6 @@
 class Following < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :follower, :class_name => 'User'
+
+	validates :user_id, :follower_id, presence: true
 end
--- db/migrate/20150401090734_create_followings.rb
+++ db/migrate/20150401090734_create_followings.rb
@@ -1,10 +1,10 @@
 class CreateFollowings < ActiveRecord::Migration
   def change
   	create_table :followings do |t|
-  		t.integer :user_id
-  		t.integer :follower_id
+  		t.integer :user_id, null: false
+  		t.integer :follower_id, null: false
 
-  		t.timestamps
+  		t.timestamps, null: false
   	end
   end
 end
