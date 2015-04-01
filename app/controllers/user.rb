c4f34c330aba64c6ee1213caa6943bc6832dc380
mbouzi <mrbouzi@gmail.com>
2015-04-01 09:46:20 -0400

    user/auth/index controllers

---
+++ app/controllers/user.rb
@@ -0,0 +1,32 @@
+get '/users/:id' do
+	@user = User.find_by(:id => params[:id])
+	erb :'users/show'
+end
+
+get '/users/home/:id' do
+	@user = User.find_by(:id => params[:id])
+	erb :'users/index'
+end
+
+
+
+
+put '/users/:id' do
+	@user = User.find_by(:id => params[:id])
+	
+	if @user
+		@user.user_name = params[:user_name]
+		@user.email = params[:email]
+		@user.bio = params[:bio]
+		@user.header_image = params[:header_image]
+		@user.avatar = params[:avatar]
+
+		if @user.save
+			redirect "/users/#{params[:id]}"
+		else
+			[500, 'something went wrong']
+		end
+	else
+		[404, 'that user does not exist']
+	end
+end
