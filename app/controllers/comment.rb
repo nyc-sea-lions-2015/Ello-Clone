21a8162ac0cbfc3c236bbf1f299b8f841e56b44d
mbouzi <mrbouzi@gmail.com>
2015-04-01 10:00:18 -0400

    comment controller

---
+++ app/controllers/comment.rb
@@ -0,0 +1,21 @@
+get '/posts/:id/comments/new' do
+	@post = Post.find_by(:id => params[:id])
+	erb :"comments/new"#, layout: !request.xhr?
+end
+
+post '/posts/:id/comments/new' do
+	@post = Post.find_by(:id => params[:id])
+	@comment =  Comment.new( content: params[:content],
+                             user_id: session[:user_id],
+                             post_id: @post.id) 
+
+	if @comment.save
+		if request.xhr?
+			erb :_new_comment, layout: false, locals: {comment: @comment}
+		else 
+		redirect "/posts/#{params[:id]}/comments"
+		end
+	else
+		[404, 'This comment could not be created']
+	end
+end
