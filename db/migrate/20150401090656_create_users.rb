eb74f26b35ddb5e97312d67c71c06859fb8b6979
mbouzi <mrbouzi@gmail.com>
2015-04-01 09:11:23 -0400

    users migrate without constraints

--- db/migrate/20150401090656_create_users.rb
+++ db/migrate/20150401090656_create_users.rb
@@ -1,4 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+  	create_able :users do |t|
+  		t.string :email
+  		t.string :password_digest
+  		t.string :name
+  		t.string :bio
+  		t.binary :header_image
+  		t.binary :avatar
+  	end
   end
 end
