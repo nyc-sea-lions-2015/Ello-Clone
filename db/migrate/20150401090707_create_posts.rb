2d5fff839ae2f4e2339e4f2f69a0f443417439c3
mbouzi <mrbouzi@gmail.com>
2015-04-01 09:33:45 -0400

    post contraints/validations

--- db/migrate/20150401090707_create_posts.rb
+++ db/migrate/20150401090707_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def change
   	create_table :posts do |t|
-  		t.belongs_to :user
-  		t.binary :picture
-  		t.text :content
+  		t.belongs_to :user, null: false
+  		t.binary :picture, null: false
+  		t.text :content, null: true
 
-  		t.timestamps
+  		t.timestamps, null: false
   	end
   end
 end
