c680135bfebb8504c7ccec9c906b58efeee64451
mbouzi <mrbouzi@gmail.com>
2015-04-01 09:22:43 -0400

    post/user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+	has_many :comments
+	has_many :posts
+	has_many :followers, :class_name => 'Followings', :foreign_key => 'user_id'
+	has_many :following, :class_name => 'Followings', :foreign_key => 'follower_id' 
+
+	has_secure_password
 end
--- db/migrate/20150401090734_create_followings.rb
+++ db/migrate/20150401090734_create_followings.rb
@@ -1,7 +1,7 @@
 class CreateFollowings < ActiveRecord::Migration
   def change
   	create_table :followings do |t|
-  		t.integer :person_id
+  		t.integer :user_id
   		t.integer :follower_id
 
   		t.timestamps
