8a4b78ffcf4bde68eb2b23511f33a05acf3ec29a
mbouzi <mrbouzi@gmail.com>
2015-04-01 14:56:28 -0400

    index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 me = User.create({:email => 'mrbouzi', :user_name => 'mbouzi', :password => 'libery13'})
-jen = User.create({:email => 'jrbouzi', :user_name => 'jbouzi', :password => 'libery12'})
+jen = User.create({:email => 'jrbouzi', :user_name => 'jbouzi', :password => 'libery12', :avatar =>('http://www.afcb.co.uk/images/common/bg_player_profile_default_big.png'), :header_image =>('http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg')})
 
 Post.create({:image => ('http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg'), :user_id => 2 })
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#add_comment').click(function(event){
+    event.preventDefault();
+
+    $(this).hide();
+
+    var url = $(this).attr('href');
+   
+
+    $.get(url, function(serverResponse, status, request){
+      $('#comment_section').append(serverResponse);
+    });
+  });
+
+  $('#comment_section').on('submit', '#new_comment', function(event){
+    event.preventDefault();
+
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
+
+    $.post(url, data, function(serverResponse, status, request){
+      $('#comment_section').append(serverResponse);
+    });
+
+    $(this).parent('div').remove();
+
+    $('#add_comment').show();
+  });
+
 });
